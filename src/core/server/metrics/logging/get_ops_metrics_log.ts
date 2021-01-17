dcd8ff1325897b9535fb39613c3ef197ad638a50
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-01-17 13:29:10 -0700

    moves opsMetrics getter to new file

---
+++ src/core/server/metrics/logging/get_ops_metrics_log.ts
@@ -0,0 +1,48 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import numeral from '@elastic/numeral';
+import { LogMeta } from '@kbn/logging';
+import { OpsMetrics } from '..';
+
+export function getEcsOpsMetricsLog({ process, os }: Partial<OpsMetrics>): LogMeta {
+  // NOTE: verbose refactor to only return parts that are defined.
+  // TODO: provide these metrics in a structured ECS-compatible way
+  const memoryLogValueinMB = process?.memory?.heap?.used_in_bytes
+    ? numeral(process?.memory?.heap?.used_in_bytes).format('0.0b')
+    : undefined;
+  // ProcessMetricsCollector converts from seconds to milliseconds. Format here is HH:mm:ss for backward compatibility
+  const uptimeLogValue = process?.uptime_in_millis
+    ? numeral(process?.uptime_in_millis / 1000).format('00:00:00')
+    : undefined;
+
+  const delayLogValue = process?.event_loop_delay
+    ? numeral(process?.event_loop_delay).format('0.000')
+    : undefined;
+
+  const loadLogValue = [...Object.values(os?.load ?? [])].map((val: number) => {
+    return numeral(val).format('0.00');
+  });
+  const meta = {
+    ...(memoryLogValueinMB && { memory: memoryLogValueinMB }),
+    ...(uptimeLogValue && { uptime: uptimeLogValue }),
+    ...(loadLogValue.length > 0 && { load: loadLogValue }),
+    ...(delayLogValue && { delay: delayLogValue }),
+  };
+  return meta;
+}
--- src/core/server/metrics/metrics_service.ts
+++ src/core/server/metrics/metrics_service.ts
@@ -21,11 +21,12 @@ import { ReplaySubject } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { CoreService } from '../../types';
 import { CoreContext } from '../core_context';
-import { Logger, LogMeta } from '../logging';
+import { Logger } from '../logging';
 import { InternalHttpServiceSetup } from '../http';
 import { InternalMetricsServiceSetup, InternalMetricsServiceStart, OpsMetrics } from './types';
 import { OpsMetricsCollector } from './ops_metrics_collector';
 import { opsConfig, OpsConfigType } from './ops_config';
+import { getEcsOpsMetricsLog } from './logging';
 
 interface MetricsServiceSetupDeps {
   http: InternalHttpServiceSetup;
@@ -80,39 +81,11 @@ export class MetricsService
 
     return this.service;
   }
-  private extractOpsLogsData({ process, os }: Partial<OpsMetrics>): LogMeta {
-    // NOTE: verbose refactor to only return parts that are defined.
-
-    // TODO: provide these metrics in a structured ECS-compatible way
-    // let logMessageResponses = '';
-    const memoryLogValueinMB = process?.memory?.heap?.used_in_bytes
-      ? numeral(process?.memory?.heap?.used_in_bytes).format('0.0b')
-      : undefined;
-    // ProcessMetricsCollector converts from seconds to milliseconds. Format here is HH:mm:ss for backward compatibility
-    const uptimeLogValue = process?.uptime_in_millis
-      ? numeral(process?.uptime_in_millis / 1000).format('00:00:00')
-      : undefined;
-
-    const delayLogValue = process?.event_loop_delay
-      ? numeral(process?.event_loop_delay).format('0.000')
-      : undefined;
-
-    const loadLogValue = [...Object.values(os?.load ?? [])].map((val: number) => {
-      return numeral(val).format('0.00');
-    });
-    const opsMetricsLogMeta = {
-      ...(memoryLogValueinMB && { memory: memoryLogValueinMB }),
-      ...(uptimeLogValue && { uptime: uptimeLogValue }),
-      ...(loadLogValue.length > 0 && { load: loadLogValue }),
-      ...(delayLogValue && { delay: delayLogValue }),
-    };
-    return opsMetricsLogMeta;
-  }
 
   private async refreshMetrics() {
     this.logger.debug('Refreshing metrics');
     const metrics = await this.metricsCollector!.collect();
-    const opsLogsMetricsMeta = this.extractOpsLogsData(metrics);
+    const opsLogsMetricsMeta = getEcsOpsMetricsLog(metrics);
     // TODO: refactor to report the metrics as a meta property:
     this.opsMetricsLogger.debug('ops metrics', opsLogsMetricsMeta);
     this.metricsCollector!.reset();
