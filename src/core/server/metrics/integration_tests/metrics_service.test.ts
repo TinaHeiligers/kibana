75d1fcfc93ea88d6807506101f853d8a7e6a176e
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-01-20 07:57:23 -0700

    Changes get_ops_metrics_log unit test fixtures

--- src/core/server/metrics/integration_tests/metrics_service.test.ts
+++ src/core/server/metrics/integration_tests/metrics_service.test.ts
@@ -1,21 +1,11 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * and the Server Side Public License, v 1; you may not use this file except in
+ * compliance with, at your election, the Elastic License or the Server Side
+ * Public License, v 1.
  */
+
 import numeral from '@elastic/numeral';
 import { OpsMetrics } from '..';
 import * as kbnTestServer from '../../../test_helpers/kbn_server';
@@ -49,40 +39,10 @@ const otherTestSettings = {
   },
 };
 
-function extractTestMetricsOfInterest({ process, os }: Partial<OpsMetrics>): LogMeta {
-  // NOTE: verbose refactor to only return parts that are defined.
-
-  // TODO: provide these metrics in a structured ECS-compatible way
-  // let logMessageResponses = '';
-  const memoryLogValueinMB = process?.memory?.heap?.used_in_bytes
-    ? numeral(process?.memory?.heap?.used_in_bytes).format('0.0b')
-    : undefined;
-  // ProcessMetricsCollector converts from seconds to milliseconds. Format here is HH:mm:ss for backward compatibility
-  const uptimeLogValue = process?.uptime_in_millis
-    ? numeral(process?.uptime_in_millis / 1000).format('00:00:00')
-    : undefined;
-
-  const delayLogValue = process?.event_loop_delay
-    ? numeral(process?.event_loop_delay).format('0.000')
-    : undefined;
-
-  const loadLogValue = [...Object.values(os?.load ?? [])].map((val: number) => {
-    return numeral(val).format('0.00');
-  });
-  const opsMetricsLogMetaMessage = {
-    ...(memoryLogValueinMB && { memory: memoryLogValueinMB }),
-    ...(uptimeLogValue && { uptime: uptimeLogValue }),
-    ...(loadLogValue.length > 0 && { load: loadLogValue }),
-    ...(delayLogValue && { delay: delayLogValue }),
-  };
-  return opsMetricsLogMetaMessage;
-}
-
 describe('metrics service', () => {
   let root: Root;
   let coreSetup: InternalCoreSetup;
   let mockConsoleLog: jest.SpyInstance;
-  let testData: Partial<OpsMetrics>;
 
   describe('setup', () => {
     beforeAll(async () => {
--- src/core/server/metrics/logging/get_ops_metrics_log.test.ts
+++ src/core/server/metrics/logging/get_ops_metrics_log.test.ts
@@ -1,41 +1,61 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * and the Server Side Public License, v 1; you may not use this file except in
+ * compliance with, at your election, the Elastic License or the Server Side
+ * Public License, v 1.
  */
 
 import { getEcsOpsMetricsLog } from './get_ops_metrics_log';
 
+const createBaseOpsMetrics = () => ({
+  collected_at: new Date('2020-01-01 01:00:00'),
+  process: {
+    memory: {
+      heap: { total_in_bytes: 1, used_in_bytes: 1, size_limit: 1 },
+      resident_set_size_in_bytes: 1,
+    },
+    event_loop_delay: 1,
+    pid: 1,
+    uptime_in_millis: 1,
+  },
+  os: {
+    platform: 'darwin' as const,
+    platformRelease: 'test',
+    load: { '1m': 1, '5m': 1, '15m': 1 },
+    memory: { total_in_bytes: 1, free_in_bytes: 1, used_in_bytes: 1 },
+    uptime_in_millis: 1,
+  },
+  response_times: { avg_in_millis: 1, max_in_millis: 1 },
+  requests: { disconnects: 1, total: 1, statusCodes: { '200': 1 } },
+  concurrent_connections: 1,
+});
+
+function createMockTestData(testMetrics: any) {
+  const base = createBaseOpsMetrics();
+  return {
+    ...base,
+    ...testMetrics,
+  };
+}
+const firstMetrics = {
+  process: {
+    memory: { heap: { used_in_bytes: 100 } },
+    uptime_in_millis: 1500,
+    event_loop_delay: 50,
+  },
+  os: {
+    load: {
+      '1m': 10,
+      '5m': 20,
+      '15m': 30,
+    },
+  },
+};
+
 describe('getEcsOpsMetricsLog', () => {
   it('provides correctly formatted message', () => {
-    const firstMetrics = {
-      process: {
-        memory: { heap: { used_in_bytes: 100 } },
-        uptime_in_millis: 1500,
-        event_loop_delay: 50,
-      },
-      os: {
-        load: {
-          '1m': 10,
-          '5m': 20,
-          '15m': 30,
-        },
-      },
-    };
-    const result = getEcsOpsMetricsLog(firstMetrics);
+    const result = getEcsOpsMetricsLog(createMockTestData(firstMetrics));
     expect(result.message).toMatchInlineSnapshot(
       `"{\\"memory\\":\\"100.0B\\",\\"uptime\\":\\"0:00:01\\",\\"load\\":[\\"10.00\\",\\"20.00\\",\\"30.00\\"],\\"delay\\":\\"50.000\\"}"`
     );
