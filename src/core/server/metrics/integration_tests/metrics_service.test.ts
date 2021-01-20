cea1765f8a80820b913a004c13dad94c6372a170
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-01-20 13:44:25 -0700

    Adds logging config integration tests

--- src/core/server/metrics/integration_tests/metrics_service.test.ts
+++ src/core/server/metrics/integration_tests/metrics_service.test.ts
@@ -27,6 +27,10 @@ const otherTestSettings = {
         },
       },
     },
+    root: {
+      appenders: ['custom-console', 'default'],
+      level: 'warn',
+    },
     loggers: [
       {
         context: 'metrics.ops',
@@ -34,6 +38,9 @@ const otherTestSettings = {
         level: 'debug',
       },
     ],
+  },
+  plugins: {
+    initialize: false,
   },
 };
 
@@ -43,34 +50,107 @@ describe('metrics service', () => {
   let mockConsoleLog: jest.SpyInstance;
   let testData: OpsMetrics;
 
+  beforeAll(async () => {
+    mockConsoleLog = jest.spyOn(global.console, 'log');
+    mockConsoleLog.mockClear();
+  });
+
+  afterEach(() => {
+    mockConsoleLog.mockClear();
+  });
+
+  afterAll(async () => {
+    mockConsoleLog.mockRestore();
+    if (root) {
+      await root.shutdown();
+    }
+  });
+
   describe('setup', () => {
-    beforeAll(async () => {
-      mockConsoleLog = jest.spyOn(global.console, 'log');
-      mockConsoleLog.mockClear();
+    it('returns ops interval and getOpsMetrics$ observable', async () => {
       root = kbnTestServer.createRoot({ ...otherTestSettings });
       coreSetup = await root.setup();
-    });
-
-    afterAll(async () => {
-      await root.shutdown();
-      mockConsoleLog.mockRestore();
-    });
-
-    it('returns ops interval and getOpsMetrics$ observable', async () => {
       expect(coreSetup.metrics).toHaveProperty(
         'collectionInterval',
         otherTestSettings.ops.interval
       );
       expect(coreSetup.metrics).toHaveProperty('getOpsMetrics$');
+      await root.shutdown();
+    });
+  });
+
+  describe('ops metrics logging configuration', () => {
+    it('does not log with logging set to quiet', async () => {
+      root = kbnTestServer.createRoot({ logging: { quiet: true } });
+      coreSetup = await root.setup();
+
+      coreSetup.metrics.getOpsMetrics$().subscribe((opsMetrics) => {
+        testData = opsMetrics;
+      });
+
+      expect(mockConsoleLog).not.toHaveBeenCalled();
+      await root.shutdown();
     });
 
-    it('logs memory, uptime, load and delay ops metrics', async () => {
+    it('logs at the correct level and with the correct context', async () => {
+      const testSettings = {
+        ops: {
+          interval: 500,
+        },
+        logging: {
+          silent: true, // set "true" in kbnTestServer
+          appenders: {
+            'custom-console': {
+              kind: 'console',
+              layout: {
+                highlight: false,
+                kind: 'pattern',
+                pattern: '%level|%logger',
+              },
+            },
+          },
+          root: {
+            appenders: ['custom-console', 'default'],
+            level: 'warn',
+          },
+          loggers: [
+            {
+              context: 'metrics.ops',
+              appenders: ['custom-console'],
+              level: 'debug',
+            },
+          ],
+        },
+        plugins: {
+          initialize: false,
+        },
+      };
+      root = kbnTestServer.createRoot({ ...testSettings });
+      coreSetup = await root.setup();
+
+      coreSetup.metrics.getOpsMetrics$().subscribe((opsMetrics) => {
+        testData = opsMetrics;
+      });
+
+      expect(mockConsoleLog).toHaveBeenCalledTimes(1);
+      const [level, logger] = mockConsoleLog.mock.calls[0][0].split('|');
+      expect(level).toBe('DEBUG');
+      expect(logger).toBe('metrics.ops');
+
+      await root.shutdown();
+    });
+  });
+  describe('ops metrics logging content', () => {
+    it('logs memory, uptime, load and delay ops metrics in the message', async () => {
+      root = kbnTestServer.createRoot({ ...otherTestSettings });
+      coreSetup = await root.setup();
+
       coreSetup.metrics.getOpsMetrics$().subscribe((opsMetrics) => {
         testData = opsMetrics;
       });
       const expectedArray = ['memory:', 'uptime:', 'load:', 'delay:'];
 
-      expect(testData).toBeTruthy(); // new it block
+      expect(testData).toBeTruthy();
       expect(mockConsoleLog).toHaveBeenCalledTimes(1);
       const [message] = mockConsoleLog.mock.calls[0][0].split('|');
 
@@ -79,25 +159,37 @@ describe('metrics service', () => {
       // the contents of the message are variable based on the process environment,
       // so we are only performing assertions against parts of the string
       expect(testParts).toEqual(expect.arrayContaining(expectedArray));
+
+      await root.shutdown();
     });
 
     it('logs structured data in the log meta', async () => {
+      root = kbnTestServer.createRoot({ ...otherTestSettings });
+      coreSetup = await root.setup();
+
       coreSetup.metrics.getOpsMetrics$().subscribe((opsMetrics) => {
         testData = opsMetrics;
       });
       const [, meta] = mockConsoleLog.mock.calls[0][0].split('|');
-      expect(JSON.parse(meta).kind).toBe('metric'); // new it block
-      expect(Object.keys(JSON.parse(meta).host.os.load)).toEqual(['1m', '5m', '15m']); // new it block
+      expect(Object.keys(JSON.parse(meta).host.os.load)).toEqual(['1m', '5m', '15m']);
+      expect(Object.keys(JSON.parse(meta).process)).toEqual(expect.arrayContaining(['uptime']));
+
+      await root.shutdown();
     });
 
     it('logs ECS fields in the log meta', async () => {
+      root = kbnTestServer.createRoot({ ...otherTestSettings });
+      coreSetup = await root.setup();
+
       coreSetup.metrics.getOpsMetrics$().subscribe((opsMetrics) => {
         testData = opsMetrics;
       });
       const [, meta] = mockConsoleLog.mock.calls[0][0].split('|');
-      expect(JSON.parse(meta).kind).toBe('metric'); // new it block
+      expect(JSON.parse(meta).kind).toBe('metric');
       expect(JSON.parse(meta).ecs.version).toBe('1.7.0');
       expect(JSON.parse(meta).category).toEqual(expect.arrayContaining(['process', 'host']));
+
+      await root.shutdown();
     });
   });
 });
