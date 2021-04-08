aead4a6b05caab661fb41057be95adcca3da0860
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-04-07 17:10:12 -0700

    logs corrupt saved objects transform errors rather than throwing

--- src/core/server/saved_objects/migrationsv2/migrations_state_action_machine.ts
+++ src/core/server/saved_objects/migrationsv2/migrations_state_action_machine.ts
@@ -51,7 +51,7 @@ const logActionResponse = (
 ) => {
   logger.debug(logMessagePrefix + `${state.controlState} RESPONSE`, res as LogMeta);
 };
-
+// naming here is misleading, dumpExecutionLog "dumps" the log messages, it doesn't delete them.
 const dumpExecutionLog = (logger: Logger, logMessagePrefix: string, executionLog: ExecutionLog) => {
   logger.error(logMessagePrefix + 'migration failed, dumping execution log:');
   executionLog.forEach((log) => {
@@ -140,7 +140,6 @@ export async function migrationStateActionMachine({
       }
     } else if (finalState.controlState === 'FATAL') {
       dumpExecutionLog(logger, logMessagePrefix, executionLog);
-      // store this error and return the collection on final failure
       return Promise.reject(
         new Error(
           `Unable to complete saved object migrations for the [${initialState.indexPrefix}] index: ` +
@@ -152,12 +151,10 @@ export async function migrationStateActionMachine({
     }
   } catch (e) {
     if (e instanceof EsErrors.ResponseError) {
-      // store this error and return the collection on final failure
       logger.error(
         logMessagePrefix + `[${e.body?.error?.type}]: ${e.body?.error?.reason ?? e.message}`
       );
       dumpExecutionLog(logger, logMessagePrefix, executionLog);
-      // store this error and return the collection on final failure
       throw new Error(
         `Unable to complete saved object migrations for the [${
           initialState.indexPrefix
@@ -170,7 +167,6 @@ export async function migrationStateActionMachine({
 
       dumpExecutionLog(logger, logMessagePrefix, executionLog);
       if (e instanceof CorruptSavedObjectError) {
-        // store this error and return the collection on final failure
         throw new Error(
           `${e.message} To allow migrations to proceed, please delete this document from the [${initialState.indexPrefix}_${initialState.kibanaVersion}_001] index.`
         );
