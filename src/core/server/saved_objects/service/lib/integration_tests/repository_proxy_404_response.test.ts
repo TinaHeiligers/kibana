f09084fcb70f9e382578b447978415898fb18724
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-08-16 16:21:07 -0700

    More hacking to no avail

--- src/core/server/saved_objects/service/lib/integration_tests/repository_proxy_404_response.test.ts
+++ src/core/server/saved_objects/service/lib/integration_tests/repository_proxy_404_response.test.ts
@@ -15,12 +15,11 @@ describe('404s from proxies', () => {
   it('returns unavailable errors', async () => {
     // Create an ES instance
     const { startES } = kbnTestServer.createTestServers({
-      adjustTimeout: (t: number) => jest.setTimeout(t),
+      adjustTimeout: jest.setTimeout,
     });
 
     const { hosts, stop: stopES } = await startES();
     const esUrl = new URL(hosts[0]);
-
     // For the proxy, use a port number that is 100 higher than the one that the actual ES instance is using
     const proxyPort = parseInt(esUrl.port, 10) + 100;
 
@@ -32,7 +31,6 @@ describe('404s from proxies', () => {
       port: proxyPort,
     });
     await server.register(h2o2);
-
     server.route({
       method: '*',
       path: '/*',
@@ -40,73 +38,43 @@ describe('404s from proxies', () => {
         if (customResponse) {
           return h.response(customResponse.body).code(customResponse.statusCode);
         }
-        // options: https://hapi.dev/module/h2o2/api/?v=9.1.0#hproxyoptions
-        return h.proxy({ host: esUrl.hostname, port: esUrl.port, protocol: 'http' });
+
+        return h.proxy({ host: esUrl.host, port: esUrl.port, protocol: 'http' });
       },
     });
     await server.start();
 
-    // TODO: Use this for the proxy tests
-    const hostsSettings = [
-      `http://${esUrl.username}:${esUrl.password}@${esUrl.hostname}:${proxyPort}`,
-    ];
-
-    const testServerSettings = {
-      elasticsearch: {
-        // hosts: [`http://elastic:changeme@localhost:9220/`], // test what it should be, hard-coded
-        // hosts: [`http://${esUrl.host}:${proxyPort}`], // original from Josh's suggestion-> here host includes the port so we're duplicating the port part and we get an error.
-        // hosts: [`http://${esUrl.hostname}:${proxyPort}`], // modified to only specify the port once.
-        hosts: hostsSettings, // my hack
-      },
-    };
-
-    console.log('testServerSettings', testServerSettings);
     // Setup kibana configured to use proxy as ES backend
-
-    // What it should be from Josh's comment:
     const root = kbnTestServer.createRoot({
-      ...testServerSettings,
+      elasticsearch: { hosts: [`http://${esUrl.host}:${proxyPort}`] },
     });
     await root.preboot();
     const setup = await root.setup();
-    // register a saved object type to check
     setup.savedObjects.registerType({
-      name: 'MyType',
       hidden: false,
-      namespaceType: 'single',
       mappings: {
         dynamic: false,
-        properties: {
-          textField: {
-            type: 'text',
-          },
-          boolField: {
-            type: 'boolean',
-          },
-        },
+        properties: {},
       },
+      name: 'test_counter_type',
+      namespaceType: 'single'
     });
 
     const { savedObjects } = await root.start();
 
     // Get a saved object repository
     const repository = savedObjects.createInternalRepository();
-    try {
-      const response = await repository.get('MyType', '123');
-      // console.log('response:', response);
-    } catch (err) {
-      expect(err.output.statusCode).toBe(404);
-      expect(err.output.payload.error).toBe('Not Found');
 
-      // expect(err).toBeInstanceOf(SavedObjectsErrorHelpers.createGenericNotFoundError);
-    }
-    // const response = await repository.get('MyType', '123');
-    // console.log('do we get a response?', response);
-    // expect(!!response).toBeTruthy();
-    // // expect(response).toEqual(...);
+
+    // set a custom response
+    customResponse = { body: { reason: 'proxy response!' }, statusCode: 404 };
+
+    // ... do something with repository
+    const response = await repository.get('test_counter_type');
+    expect(response).toEqual(...);
 
     // unset custom response (should fallback to ES now again)
-    // customResponse = undefined;
+    customResponse = undefined;
 
     await root.shutdown();
     await server.stop();
