fce09754a953a48b0d1004e5d88aa4b6104859e3
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-08-20 18:52:59 -0700

    Adds bulkCreate tests

--- src/core/server/saved_objects/service/lib/integration_tests/repository_with_proxy.test.ts
+++ src/core/server/saved_objects/service/lib/integration_tests/repository_with_proxy.test.ts
@@ -13,6 +13,8 @@ import { InternalCoreSetup, InternalCoreStart } from '../../../../internal_types
 import { Root } from '../../../../root';
 import * as kbnTestServer from '../../../../../test_helpers/kbn_server';
 import { SavedObjectsErrorHelpers } from '../errors';
+import { ISavedObjectsRepository } from '../repository';
+import { SavedObject } from '../../../types';
 
 let esServer: kbnTestServer.TestElasticsearchUtils;
 
@@ -50,6 +52,7 @@ const registerSOTypes = (setup: InternalCoreSetup) => {
     namespaceType: 'single',
   });
 };
+let triggerTest: string | undefined;
 
 describe('404s from proxies', () => {
   let root: Root;
@@ -70,6 +73,7 @@ describe('404s from proxies', () => {
       port: proxyPort,
     });
     await hapiServer.register(h2o2);
+
     // register routes, specific ones to modify the response and a catch-all to relay the request/response
     hapiServer.route({
       method: 'GET',
@@ -97,6 +101,62 @@ describe('404s from proxies', () => {
       },
     });
 
+    // POST _bulk route
+    hapiServer.route({
+      method: 'POST',
+      path: '/_bulk',
+      options: {
+        payload: {
+          output: 'data',
+          parse: false,
+        },
+        handler: (req, h) => {
+          if (triggerTest === 'bulkCreate') {
+            return h.proxy({
+              ...defaultProxyOptions(esUrl.hostname, esUrl.port),
+              // eslint-disable-next-line @typescript-eslint/no-shadow
+              onResponse: async (err, res, request, h, settings, ttl) => {
+                // console.log('handling response', res);
+                const response = h
+                  .response(res)
+                  .header('x-elastic-product', 'somethingitshouldnotbe', { override: true }) // changes the product header
+                  .code(404); // returning 404 throws the 404 from the repository call
+                return response;
+              },
+            });
+          } else {
+            return relayHandler(h, esUrl.hostname, esUrl.port);
+          }
+        },
+      },
+    });
+    // GET _search route
+    hapiServer.route({
+      method: 'GET',
+      path: '/.kibana_8.0.0/_search',
+      options: {
+        handler: (req, h) => {
+          const payload = req.payload;
+          if (!payload) {
+            return h.proxy({
+              ...defaultProxyOptions(esUrl.hostname, esUrl.port),
+              // eslint-disable-next-line @typescript-eslint/no-shadow
+              onResponse: async (err, res, request, h, settings, ttl) => {
+                const response = h
+                  .response(res)
+                  .header('x-elastic-product', 'somethingitshouldnotbe', { override: true })
+                  .code(404);
+                return response;
+              },
+            });
+          } else {
+            return relayHandler(h, esUrl.hostname, esUrl.port);
+          }
+        },
+      },
+    });
+
+    // POST _update
     hapiServer.route({
       method: 'POST',
       path: '/.kibana_8.0.0/_update/{_id*}', // I only want to match on part of a param
@@ -108,6 +168,7 @@ describe('404s from proxies', () => {
         handler: (req, h) => {
           // options: https://hapi.dev/module/req.params.typeh2o2/api/?v=9.1.0#hproxyoptions
           if (req.params._id.startsWith('my_type:')) {
+            // I could create a set of params to match against to use as the throwing handler trigger
             // mimics a 404 'unexpected' response from the proxy
             return h.proxy({
               ...defaultProxyOptions(esUrl.hostname, esUrl.port),
@@ -117,10 +178,7 @@ describe('404s from proxies', () => {
                 const response = h
                   .response(res)
                   .header('x-elastic-product', 'somethingitshouldnotbe', { override: true }) // changes the product header
-                  // .code(418); // returning teapot code causes repository to throw 500
                   .code(404); // returning 404 throws the 404 from the repository call
-                // .code(200); // returning 200 catches the issue and the repository throws a 503 as expected (when the 404 status code check is removed)
-                // console.log('the response from the proxy is:', response);
                 return response;
               },
             });
@@ -131,6 +189,7 @@ describe('404s from proxies', () => {
       },
     });
 
+    // catch-all passthrough route
     hapiServer.route({
       method: '*',
       path: '/{any*}',
@@ -172,45 +231,92 @@ describe('404s from proxies', () => {
       await esServer.stop();
     }
   });
-  // my_other_type test
-  it('does not alter a Not Found response if the document does not exist and the proxy returns the correct product header', async () => {
-    const repository = start.savedObjects.createInternalRepository();
-    const myOtherType = await repository.create('my_other_type', {
-      _id: '123',
-      namespace: 'default',
-      references: [],
-      attributes: {
-        title: 'my_other_type1',
-      },
+  // my_other_type test (proxy relays request/response as is)
+  describe('requests when a proxy relays request/responses with the correct product header', () => {
+    let repository: ISavedObjectsRepository;
+    let myOtherType: SavedObject;
+
+    beforeAll(async () => {
+      repository = start.savedObjects.createInternalRepository();
+      myOtherType = await repository.create('my_other_type', {
+        _id: '123',
+        namespace: 'default',
+        references: [],
+        attributes: {
+          title: 'my_other_type1',
+        },
+      });
     });
-    if (myOtherType) {
+
+    it('does not alter a Not Found response if the document does not exist and the proxy returns the correct product header', async () => {
+      let customErr: any;
       try {
         await repository.get('my_other_type', '123');
       } catch (err) {
-        expect(err.output.statusCode).toBe(404);
-        expect(err.output.payload.message).toBe('Saved object [my_other_type/123] not found');
+        customErr = err;
       }
-    }
-  });
-  it('returns a document if it exists and if the proxy passes through the product header', async () => {
-    const repository = start.savedObjects.createInternalRepository();
-    const myOtherType = await repository.create('my_other_type', {
-      namespace: 'default',
-      references: [],
-      attributes: {
-        title: 'my_other_type1',
-      },
+      expect(customErr?.output?.statusCode).toBe(404);
+      expect(customErr.output.payload.message).toBe('Saved object [my_other_type/123] not found');
     });
-    const myOtherTypeDoc = await repository.get('my_other_type', `${myOtherType.id}`); // document exists and proxy passes the response through unmodified
-    expect(myOtherTypeDoc.type).toBe('my_other_type');
+
+    it('returns a document if it exists and if the proxy passes through the product header', async () => {
+      const myOtherTypeDoc = await repository.get('my_other_type', `${myOtherType.id}`); // document exists and proxy passes the response through unmodified
+      expect(myOtherTypeDoc.type).toBe('my_other_type');
+    });
+
+    it('handles update requests that are successful', async () => {
+      const docToUpdate = await repository.create('my_other_type', {
+        namespace: 'default',
+        references: [],
+        attributes: {
+          title: 'original title',
+        },
+      });
+      const updatedDoc = await repository.update('my_other_type', `${docToUpdate.id}`, {
+        title: 'updated title',
+      });
+      expect(updatedDoc.type).toBe('my_other_type');
+      expect(updatedDoc.attributes.title).toBe('updated title');
+    });
+
+    it('handles bulkCreate requests when the proxy relays request/responses correctly', async () => {
+      triggerTest = null;
+      const bulkObjects = [
+        {
+          type: 'my_other_type',
+          id: '1',
+          attributes: {
+            title: 'bulkType1',
+          },
+          references: [],
+        },
+        {
+          type: 'my_other_type',
+          id: '2',
+          attributes: {
+            title: 'bulkType2',
+          },
+          references: [],
+        },
+      ];
+      if (triggerTest === null) {
+        const bulkResponse = await repository.bulkCreate(bulkObjects, { namespace: 'default' });
+        expect(bulkResponse.saved_objects.length).toEqual(2);
+      }
+      expect(true); // force test exit.
+    });
   });
-  // my_type test
-  // TODO: create a mocked Stream response using Readable.from(JSON.stringify({...SO doc}))
-  // See src/core/server/elasticsearch/client/configure_client.test.ts
-  it('returns an EsUnavailable error if the document exists but the proxy cannot find the es node (mimics allocator changes)', async () => {
-    const repository = start.savedObjects.createInternalRepository();
-    let myType: any;
-    try {
+
+  describe('requests when a proxy returns Not Found with an incorrect product header', () => {
+    let repository: ISavedObjectsRepository;
+    let myType: SavedObject;
+
+    beforeAll(async () => {
+      repository = start.savedObjects.createInternalRepository();
+    });
+
+    it('returns an EsUnavailable error if the document exists but the proxy cannot find the es node (mimics allocator changes)', async () => {
+      let myError;
       myType = await repository.create('my_type', {
         _id: '123',
         namespace: 'default',
@@ -219,54 +325,69 @@ describe('404s from proxies', () => {
           title: 'my_type1',
         },
       });
-      if (myType) {
-        await repository.get('my_type', `${myType.id}`); // document doesn't exist and the proxy modifies the response header -> TODO the doc should exist but the proxy cannot find the es node
+      try {
+        await repository.get('my_type', `${myType.id}`);
+      } catch (err) {
+        myError = err;
       }
-    } catch (err) {
-      expect(err.output.statusCode).toBe(503);
-      expect(err.output.payload.message).toBe(
+      expect(myError.output.statusCode).toBe(503);
+      expect(myError.output.payload.message).toBe(
         `x-elastic-product not present or not recognized: Saved object [my_type/${myType.id}] not found`
       );
-    }
-  });
-
-  it('handles update requests that are successful', async () => {
-    const repository = start.savedObjects.createInternalRepository();
-    const docToUpdate = await repository.create('my_other_type', {
-      namespace: 'default',
-      references: [],
-      attributes: {
-        title: 'original title',
-      },
-    });
-    const updatedDoc = await repository.update('my_other_type', `${docToUpdate.id}`, {
-      title: 'updated title',
-    });
-    expect(updatedDoc.type).toBe('my_other_type');
-    expect(updatedDoc.attributes.title).toBe('updated title');
-  });
-
-  it('handles update requests that are interrupted', async () => {
-    const repository = start.savedObjects.createInternalRepository();
-    const docToUpdate = await repository.create('my_type', {
-      namespace: 'default',
-      references: [],
-      attributes: {
-        title: 'original title',
-      },
     });
 
-    try {
-      await repository.update('my_type', `${docToUpdate.id}`, {
-        title: 'updated title',
+    it('returns an EsUnavailable error on update requests that are interrupted', async () => {
+      let myError;
+      const docToUpdate = await repository.create('my_type', {
+        namespace: 'default',
+        references: [],
+        attributes: {
+          title: 'original title',
+        },
       });
-      expect(false).toBe(true); // Should not get here (we expect the call to throw)
-    } catch (err) {
-      expect(err).toBeInstanceOf(SavedObjectsErrorHelpers.decorateEsUnavailableError);
-      expect(err.output.statusCode).toBe(503);
-      expect(err.output.payload.message).toBe(
+      try {
+        await repository.update('my_type', `${docToUpdate.id}`, {
+          title: 'updated title',
+        });
+        expect(false).toBe(true); // Should not get here (we expect the call to throw)
+      } catch (err) {
+        myError = err;
+      }
+      expect(myError?.output?.statusCode).toBe(503);
+      expect(myError.output.payload.message).toBe(
         'x-elastic-product not present or not recognized: Not Found'
       );
-    }
+    });
+
+    it('returns an EsUnavailable error on bulkCreate requests when the proxy response with 404 and the incorrect product header', async () => {
+      triggerTest = 'bulkCreate';
+      let bulkCreateError: any;
+      const bulkObjects = [
+        {
+          type: 'my_type',
+          id: '1',
+          attributes: {
+            title: 'bulkType1',
+          },
+          references: [],
+        },
+        {
+          type: 'my_type',
+          id: '2',
+          attributes: {
+            title: 'bulkType2',
+          },
+          references: [],
+        },
+      ];
+      try {
+        await repository.bulkCreate(bulkObjects, { namespace: 'default' });
+      } catch (err) {
+        bulkCreateError = err;
+      }
+      expect(bulkCreateError?.output?.statusCode).toEqual(503);
+      triggerTest = null;
+      expect(triggerTest).toBe(null); // ensure a reset
+    });
   });
 });
